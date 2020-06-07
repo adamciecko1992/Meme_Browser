0e144ba342a59be158a4c57d8473b87ceeb811f5
adamciecko1992 <adam.ciecko@hotmail.com>
2020-06-07 23:02:48 +0200

    docs for guthub pages

---
+++ docs/precache-manifest.98dafadeb52ba4759a2fa9786f72aae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17816922c63909b8820c6919a24f5eb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6cf51e790b9d9a03716",
+    "url": "/static/css/main.b1fc49c2.chunk.css"
+  },
+  {
+    "revision": "c4ea0bcd2124f7b1fa54",
+    "url": "/static/js/2.6800048e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6800048e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6cf51e790b9d9a03716",
+    "url": "/static/js/main.d79980c5.chunk.js"
+  },
+  {
+    "revision": "8be790ed7e484503d521",
+    "url": "/static/js/runtime-main.ded25bfa.js"
+  }
+]);
