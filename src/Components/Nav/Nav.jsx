7785d196fdc8f704f21433299e35c5bcacc57c3e
adamciecko1992 <adam.ciecko@hotmail.com>
2020-06-10 15:55:56 +0200

    added some buttons interactions and state hook for Nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState} from "react";
 import "./Nav.scss";
 import { NavLink } from "react-router-dom";
 
 function Nav() {
+  const [active, changeActive] = useState({ hot: '', regular:'active' });
+  const clickHandler = (e) => {
+    if (e.target.innerText === "Regular") {
+      changeActive({ hot: '', regular: "active" })
+    } else if (e.target.innerText === "Hot") {
+      changeActive({ hot: 'active', regular: "" })
+    }
+  }
   return (
+    
     <div className="Nav col-2 bg-white pt-3">
       <ul className="list-group-flush text-center">
-        <li className="list-group-item bg-primary bg-danger">
+        <li onClick={clickHandler} className={`list_button list-group-item bg-primary ${active.hot}`}>
           <NavLink className="text-white text-decoration-none" to={"/"}>
             Regular
           </NavLink>
         </li>
-        <li className="list-group-item px-0 bg-secondary">
+        <li onClick={clickHandler} className={`list_button list-group-item px-0 bg-secondary ${active.regular}`}>
           <NavLink
             className="text-white text-decoration-none"
             exact
