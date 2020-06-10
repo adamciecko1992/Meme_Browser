882b95d98d5d282a42325770c575255db83be853
adamciecko1992 <adam.ciecko@hotmail.com>
2020-06-10 16:40:36 +0200

    hash router

--- src/index.js
+++ src/index.js
@@ -6,19 +6,23 @@ import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { rootReducer } from "./Reducers/rootReducer";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    Provider store = { store } >
+    <
+    HashRouter >
+    <
+    App / >
+    <
+    /HashRouter> <
+    /Provider> <
+    /React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
